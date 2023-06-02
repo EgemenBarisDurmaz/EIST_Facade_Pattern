8bb76db7bdca3650cf7fb3b1b2c996284d7556b0
Egemen Baris Durmaz <ge85vet@mytum.de>
2023-06-02 16:55:12 +0200

    task 2

--- src/de/tum/in/ase/eist/ecommerce/ECommerceFacade.java
+++ src/de/tum/in/ase/eist/ecommerce/ECommerceFacade.java
@@ -1,5 +1,34 @@
 package de.tum.in.ase.eist.ecommerce;
 
 public class ECommerceFacade {
+     OrderController orderController;
+     AdvertisementController advertisementController;
+     ShippingController shippingController;
+     Order order;
+     String address;
 
+    public ECommerceFacade(OrderController orderController, AdvertisementController advertisementController) {
+        this.orderController = orderController;
+        this.advertisementController = advertisementController;
+    }
+    public void shipOrder(Order order, String address) {
+        this.order.setShipping(order.getShipping());
+        this.shippingController.shipOrder(order);
+    }
+
+    public void processOrder(Order order) {
+        orderController.processOrder(order);
+    }
+
+    public void processOrder(Order order, String phoneNumber) {
+        orderController.processOrder(order, phoneNumber);
+    }
+
+    public Order retrieveLatestOrder(int id) {
+        return orderController.retrieveLatestOrder(id);
+    }
+
+    public void playAdvertisement(int ageRestriction) {
+        advertisementController.playAdvertisement(ageRestriction);
+    }
 }
