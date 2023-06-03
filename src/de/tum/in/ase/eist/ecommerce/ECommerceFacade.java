5203f91be232346f921f3350e5cb381aca134888
Egemen Baris Durmaz <ge85vet@mytum.de>
2023-06-03 17:50:32 +0200

    last revision

--- src/de/tum/in/ase/eist/ecommerce/ECommerceFacade.java
+++ src/de/tum/in/ase/eist/ecommerce/ECommerceFacade.java
@@ -1,28 +1,21 @@
 package de.tum.in.ase.eist.ecommerce;
 
-import java.util.ArrayList;
-
 public class ECommerceFacade {
      private OrderController orderController;
      private AdvertisementController advertisementController;
      private ShippingController shippingController;
-     //private Order order;
-     //private String address;
+
 
     public ECommerceFacade() {
         this.orderController = new OrderController();
         this.advertisementController = new AdvertisementController();
         this.shippingController = new ShippingController();
-       // this.order = new Order(new ArrayList<>());
-        //this.address = "";
     }
 
     public ECommerceFacade(OrderController orderController, AdvertisementController advertisementController, ShippingController shippingController) {
         this.orderController = orderController;
         this.advertisementController = advertisementController;
         this.shippingController = shippingController;
-       // this.order = new Order(new ArrayList<>());
-       // this.address = "";
     }
     public void shipOrder(Order order, String address) {
         Shipping shipping = this.shippingController.createShipping(address);
